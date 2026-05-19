753d1ea34b4de23fce4a42b9ace59ac2c08fa812
fuidani <dpisani@637capital.com>
2026-05-19 07:35:48 +0300

    added my first javascript code to my personal website

---
+++ main.js
@@ -0,0 +1,49 @@
+const books = [
+    {
+        title: "I Regret Almost Everything",
+        author: "Keith McNally",
+        cover: "images/IRegretAlmostEverything.png",
+        comment: "I enjoyed this book because it is a biography of someone relatively 'normal'. Most people seem to have have interesting stories to tell that are never read.",
+        quotes: [
+            "XXXXX",
+            "XXXXX"
+        ]
+    },
+    {
+        title: "Memorie di Adriano",
+        author: "Marguerite Yourcenar",
+        cover: "images/MemorieDiAdriano.png",
+        comment: "This is one of my favourite books. I read it many many times. It is a biography of the 'good emperor' Adriano.",
+        quotes: [
+            "XXXXX",
+            "XXXXX"
+        ]
+    },
+        {
+        title: "Una vita come tante",
+        author: "Hanya Yanagihara",
+        cover: "images/UnaVitaComeTante.png",
+        comment: "Amazing book - though a bit heavy. Its a long long book - truly enjoyed it",
+        quotes: [
+            "XXXXX",
+            "XXXXX"
+        ]
+    },
+];
+
+const bookshelf = document.querySelector("#bookshelf");
+
+books.forEach(function(book) {
+    const article = document.createElement("article");
+    article.classList.add("book-card");
+
+    article.innerHTML = `
+        <img src="${book.cover}" alt="${book.title}">
+       
+        <div class="book-comment">
+            <p>${book.comment}</p>
+        </div>
+      `;
+
+    bookshelf.appendChild(article);
+});
